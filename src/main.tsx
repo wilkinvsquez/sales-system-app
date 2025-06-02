e88a362b9244ce5a9a619d8f972973d69e6583e5
wilkinvsquez <wilkinvsquez@gmail.com>
2025-06-02 10:10:11 -0600

    feat(App): general styles and configuration

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,16 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import "./index.css";
+import { AuthProvider } from "./auth/context/AuthContext.tsx";
+import { BrowserRouter } from "react-router-dom";
 import App from "./App.tsx";
 
 createRoot(document.getElementById("root")!).render(
 	<StrictMode>
-		<App />
+		<AuthProvider>
+			<BrowserRouter>
+				<App />
+			</BrowserRouter>
+		</AuthProvider>
 	</StrictMode>,
 );
